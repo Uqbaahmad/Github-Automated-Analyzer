baeefcb71939425460f5e25fa32b17f1f7c94547
Uqba Ahmad <104266403+Uqbaahmad@users.noreply.github.com>
2023-08-11 15:33:31 +0530

    Update fetch_file.py

--- fetch_file.py
+++ fetch_file.py
@@ -22,55 +22,6 @@ def calculate_complexity_with_gpt(code):
     return chain.run(code)
 
 
-# def complexity(code, filepath=None):
-#     try:
-#         complexity = radon.metrics.cc_visit(code)
-#         return complexity
-#     except Exception as e:
-#         print(e)
-#         print(f"Failed to calculate complexity for file '{filepath}'.")
-#         return 0
-
-# def calculate_complexity_of_repository(github_url, repository_name):
-#     user = github_url.split('/')[-1]
-
-#     score = 0.0
-#     response = requests.get(f'https://api.github.com/repos/{user}/{repository_name}/contents')
-
-#     if response.status_code == 200:
-#         contents = response.json()        
-#         for item in contents:
-#             print(item)
-#             if item['type'] == 'file':
-#                 file_path = item['path']
-#                 file_response = requests.get(f'https://raw.githubusercontent.com/{user}/{repository_name}/master/{file_path}')
-                
-#                 if file_response.status_code == 200:
-#                     file_contents = file_response.text
-
-#                     file_contents = re.sub(r'#.*', '', file_contents)
-
-#                     # Remove import statements
-#                     file_contents = re.sub(r'^import.*$', '', file_contents, flags=re.MULTILINE)
-#                     file_contents = re.sub(r'^from.*$', '', file_contents, flags=re.MULTILINE)
-
-#                     # Remove empty lines and leading/trailing whitespaces
-#                     file_contents = '\n'.join(line.strip() for line in file_contents.split('\n') if line.strip())
-                    
-#                     try:
-#                         if '.py' in item['name']:
-#                             score += float(calculate_complexity_with_gpt(file_contents))
-#                         else :
-#                             score += float(calculate_complexity(file_contents, file_path))
-#                     except:
-#                         score += float(calculate_complexity(file_contents, file_path))
-#                 else:
-#                     print(f"Failed to fetch contents of file '{file_path}'. Error: {file_response.text}")
-#     else:
-#         print(f"Failed to fetch contents of repository '{repository_name}'. Error: {response.text}")
-#     return score
-
-
 def fetch_github_repositories(github_url):
     # Initialize variables to store the most complex repository
     most_complex_repo = None
